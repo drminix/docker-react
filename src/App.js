8ebbaf3bc517a7e98d16c414ed98ff5cb44e7ede
sang lee <sang@revealtech.ai>
2020-04-24 00:15:42 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Updated. We can edit it. Or useit
         </a>
       </header>
     </div>
