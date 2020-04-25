5fb9c00546201f57a1f602b520edc612094a3708
sang lee <sang@revealtech.ai>
2020-04-24 20:17:41 -0700

    Updatedtesting

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Updated/i);
+  //const linkElement = getByText(/Updated/i);
   expect(linkElement).toBeInTheDocument();
 });
